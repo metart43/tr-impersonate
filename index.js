e6ede238785429a9d206a8b13113e8c840a04f28
Andrew Peace <apeace95@gmail.com>
2021-07-30 11:39:30 -0400

    get users api request

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-console.log("here");
-const usersTable = document.getElementById("users-table");
-// chrome.extension.getBackgroundPage().console.log('foo', usersTable);
+const listUsers = async () => {
+  try {
+    const usersResponse = await fetch('http://localhost:3000/api/users', {
+      method: 'GET',
+      credentials: 'include',
+      mode: 'cors',
+    });
+    const users = await usersResponse.json();
+    const orgResponse = await fetch('http://localhost:3000/api/org/current', {
+      method: 'GET',
+      credentials: 'include',
+      mode: 'cors',
+    });
+    const org = await orgResponse.json();
+    if (org && org.name && users && users.length) {
+      users.forEach(function(user) {
+        user.orgName = org.name;
+      });
+    }
+    return users;
+  }
+  catch(err) {
+    console.log('Error loading users', err);
+  };
+}
 
-console.log(usersTable);
+(async () => {
+  const users = await listUsers();
+  console.log('Users', users);
+  const usersTable = document.getElementById("users-table");
+  // chrome.extension.getBackgroundPage().console.log('foo', usersTable);
+
+  console.log(usersTable);
+})().catch(err => {
+  console.error(err);
+});
